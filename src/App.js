4cf2f069e7bcb64690e8e80bedd22b705e13941f
Bibhor Kumar <80465500+bibhorkumar@users.noreply.github.com>
2023-05-07 15:18:30 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' />
+          <Route path='/cart' />
+        </Routes>
+      </Router>
     </div>
   );
 }
