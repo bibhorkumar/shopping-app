90719910d39ea7beb3a414db9a1077ca8c26178d
Bibhor Kumar <80465500+bibhorkumar@users.noreply.github.com>
2023-05-11 10:30:04 +0530

    final build

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -2,21 +2,33 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../../context/shop-context'
 import { PRODUCTS } from '../../products'
 import { CartItem } from './CartItem'
+import './cart.css'
+import { useNavigate } from 'react-router-dom'
 
 export const Cart = () => {
-  const {cartItems} = useContext(ShopContext);
+  const {cartItems, getTotalCartAmount} = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+  const navigate = useNavigate();
   return (
     <div className='cart'>
       <div>
         <h1>Your Cart Items</h1>
       </div>
       <div className='cartItems'>
+      // eslint-disable-next-line
         {PRODUCTS.map((product)=>{
           if(cartItems[product.id] !== 0){
             return <CartItem data={product} />
           }
         })}
       </div>
+    {totalAmount >0 ?
+      <div className="checkout">
+        <p>Subtotal : ${totalAmount}</p>
+        <button onClick={()=>navigate('/')}>Continue Shopping</button>
+        <button>Checkout</button>
+      </div>
+    : <h1>Your Cart is Empty</h1> }
     </div>
   )
 }
